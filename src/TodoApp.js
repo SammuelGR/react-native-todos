7d9cefbbd1c0da543258e80e16104b35cb113292
Sammuel Reis <sammugr@hotmail.com>
2019-01-30 19:51:06 -0200

    add redux and react-redux

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import TodoForm from "./components/TodoForm";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
 
 export default class TodoApp extends Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <TodoForm />
-      </SafeAreaView>
+      <Provider store={store}>
+        <SafeAreaView style={styles.container}>
+          <TodoForm />
+        </SafeAreaView>
+      </Provider>
     );
   }
 }
---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+export const ADD_TODO = "ADD_TODO";
+export const addTodo = text => ({ payload: text, type: ADD_TODO });
